d6498acc9ef54be740450121a5ad7721c412096e
FabricioGit2024 LittleFumas <fabriciogit2024@hotmail.com>
2024-08-03 20:40:31 -0300

    configuracao inicial do projeto

---
+++ src/main/java/com/SprinDataLombok/SpringDataLombokApiApplication.java
@@ -0,0 +1,13 @@
+package com.SprinDataLombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataLombokApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataLombokApiApplication.class, args);
+	}
+
+}
